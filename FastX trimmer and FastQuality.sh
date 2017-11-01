3613c72ba98ea71355c30c78a56cef1cef6cb28a
niyaznihmath <niyaznihmath@users.noreply.github.com>
2017-11-01 17:02:00 +0100

    Add files via upload

---
+++ FastX trimmer and FastQuality.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -A b2014097
+#SBATCH -p core
+#SBATCH -n 16
+#SBATCH -t 120:00:00
+#SBATCH -J FastXTrimmer-FastQuality
+#SBATCH -o /proj/b2014097/nobackup/Niyaz/logs/trim.out
+#SBATCH -e /proj/b2014097/nobackup/Niyaz/logs/trim.err
+
+
+module load bioinfo-tools 
+module load Fastx/0.0.14
+
+echo "trimming"
+
+for x in *.gz; do
+
+        file_input="$x"
+        file_out="$x"'s'
+        gunzip -c  $file_input | fastx_trimmer -Q33 -f 15 -l 95 | fastq_quality_filter -Q 33 -q 30 -p 85 -z -o $file_out
+done;
+
+echo "done"
